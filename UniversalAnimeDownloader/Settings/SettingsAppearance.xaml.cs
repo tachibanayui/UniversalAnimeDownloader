ab1e1aef9b0c1bfc8a77eee398499ae5def4940f
quangaming2929 <quangaming2929@DESKTOP-AJC0EMN>
2018-12-24 15:09:07 +0700

    Add SettingsApearance's Appearance

--- UniversalAnimeDownloader/Settings/SettingsAppearance.xaml.cs
+++ UniversalAnimeDownloader/Settings/SettingsAppearance.xaml.cs
@@ -22,12 +22,57 @@ namespace UniversalAnimeDownloader.Settings
     public partial class SettingsAppearance : Page
     {
         public SettingsAppearanceViewModel VM;
+        private Button lastColorPicker;
 
         public SettingsAppearance()
         {
             VM = new SettingsAppearanceViewModel();
             InitializeComponent();
             DataContext = VM;
+            headingSlider.ValueChanged += ChangeFontSize;
+            heading2Slider.ValueChanged += ChangeFontSize;
+            heading3Slider.ValueChanged += ChangeFontSize;
+            heading4Slider.ValueChanged += ChangeFontSize;
+        }
+
+        private void ChooseColor(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            lastColorPicker = btn;
+            //get the current color
+            colorPicker.SelectedColor = ((btn.Content as Rectangle).Fill as SolidColorBrush).Color;
+
+            colorPickerHost.IsOpen = true;
+        }
+
+        private void CloseDialogHost(object sender, RoutedEventArgs e) => colorPickerHost.IsOpen = false;
+
+        private void AssignColor(object sender, RoutedEventArgs e)
+        {
+            
+            switch (lastColorPicker.Name)
+            {
+                case "textColor":
+                    Application.Current.Resources["ForeGroundColor"] = new SolidColorBrush((Color)colorPicker.SelectedColor); 
+                    break;
+                case "desColor":
+                    Application.Current.Resources["FileSize"] = new SolidColorBrush((Color)colorPicker.SelectedColor);
+                    break;
+                case "menubarColor":
+                    Application.Current.Resources["Primary"] = new SolidColorBrush((Color)colorPicker.SelectedColor);
+                    break;
+                default:
+                    break;
+            }
+
+            colorPickerHost.IsOpen = false;
+        }
+
+        private void ChangeFontSize(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Slider sld = sender as Slider;
+            string text = ((sld.Parent as Grid).Children[0] as TextBlock).Text;
+            Application.Current.Resources[text] = e.NewValue;
         }
     }
 }
