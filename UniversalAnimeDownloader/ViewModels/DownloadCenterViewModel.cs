0d2f00a10fa8ac64d54e6d593f6f9da30f7043e1
quangaming2929 <quangaming2929@DESKTOP-AJC0EMN>
2019-03-24 16:30:36 +0700

    Add Download Center Menu

--- UniversalAnimeDownloader/ViewModels/DownloadCenterViewModel.cs
+++ UniversalAnimeDownloader/ViewModels/DownloadCenterViewModel.cs
@@ -6,12 +6,23 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Input;
 using UADAPI;
 
 namespace UniversalAnimeDownloader.ViewModels
 {
     class DownloadCenterViewModel : BaseViewModel
     {
+        #region Commands
+        public ICommand RemoveAllCommand { get; set; }
+        public ICommand PauseAllCommand { get; set; }
+        public ICommand ResumeAllCommand { get; set; }
+        public ICommand CancelAllCommand { get; set; }
+        public ICommand PauseResumeDownloadCommand { get; set; }
+        public ICommand CancelDownloadCommand { get; set; }
+        public ICommand RemoveFromListCommand { get; set; }
+        #endregion
+
         public DownloadCenterViewModel()
         {
             
