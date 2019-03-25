0f249118f7110073350f61a49aa75f7373ed9c36
quangaming2929 <quangaming2929@DESKTOP-AJC0EMN>
2019-03-25 15:52:42 +0700

    Add no notification Icon

---
+++ UniversalAnimeDownloader/ViewModels/SmallerThanParameterToVisibilityConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace UniversalAnimeDownloader.ViewModels
+{
+    class SmallerThanParameterToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var parsedValue = double.Parse(value.ToString());
+            var parsedParameter = double.Parse(parameter.ToString());
+
+            return parsedValue < parsedParameter ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
